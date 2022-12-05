b099d3c0dc7bc408ac2a1f0995a8097a381a5c51
David Goudie <dagoudie@Davids-Mac-mini.local>
2022-12-05 00:22:28 +0000

    DG amend so confirms checkbox is clicked

--- wdio_first-test.test.js
+++ wdio_first-test.test.js
@@ -16,9 +16,13 @@ let client;
   await client.elementSendKeys(todoInput['element-6066-11e4-a52e-4f735466cecf'], 'WebDriver\uE007WebDriverIO\uE007feed chia\uE007')
 
   const todoCheckbox = await client.findElements('css selector', 'li.todo input[type="checkbox"]')
+  const todoCheckboxLabel = await client.findElements('css selector', 'li.todo label')
   await client.elementClick(todoCheckbox[1]['element-6066-11e4-a52e-4f735466cecf'])
 
-  console.log(await client.getTitle()) // outputs "Vue.js • TodoMVC"
+  const label = await client.getElementText(todoCheckboxLabel[1]['element-6066-11e4-a52e-4f735466cecf'])
+  const isClicked = await client.isElementSelected(todoCheckbox[1]['element-6066-11e4-a52e-4f735466cecf'])
+
+  console.log(`checkbox ${label} is clicked === ${isClicked}`)
 
   await client.deleteSession()
 
