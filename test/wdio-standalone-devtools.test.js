a3ed0462eeedf3f2c1023bcec0efd08698402529
dagoud <dagoudie@gmx.com>
2022-12-05 16:56:37 +0000

    DG complete chapter 2 extra 2

--- test/wdio-standalone-devtools.test.js
+++ test/wdio-standalone-devtools.test.js
@@ -4,33 +4,53 @@ let browser;
 
 (async () => {
     browser = await remote({
+      automationProtocol: 'devtools',
       capabilities: {
-        // browserName: 'chrome',
-      // 'goog:chromeOptions': {
-        // args: ['headless', 'disable-gpu']
-        // }
-        browserName: "Safari"
-      },
-      // uncomment if want to connect using chromedriver - default uses puppeteer
-      // port: 9515
+        browserName: 'chrome',
+      }
     })
     
-
     await browser.navigateTo('http://todomvc.com/examples/vue/')
     
-    const todoInput = await browser.findElement('css selector', '.new-todo')  
-    await browser.elementSendKeys(todoInput['element-6066-11e4-a52e-4f735466cecf'], 'Bring in bins\uE007Hang out washing\uE007Feed Chia\uE007')
+    // Chapter 2 - Extra 2
+    /**
+     * run Puppeteer code
+     */
+    await browser.call(async () => {
+      const puppeteer = await browser.getPuppeteer()
+      const page = (await puppeteer.pages())[0]
+            
+      await page.evaluate(() => {
+        localStorage.setItem('todos-vuejs', `[{
+                "id": 1,
+                "title": "Foo",
+                "completed": false
+              }, {
+                "id": 2,
+                "title": "Bar",
+                "completed": false
+              }, {
+                "id": 3,
+                "title": "Loo",
+                "completed": false
+              }]`);
+        });
+    })
+    
+    await browser.navigateTo('http://todomvc.com/examples/vue/')
+    
     const todoCheckbox = await browser.findElements('css selector', 'li.todo input[type="checkbox"]')
     const todoCheckboxLabel = await browser.findElements('css selector', 'li.todo label')
-    await browser.elementClick(todoCheckbox[1]['element-6066-11e4-a52e-4f735466cecf'])
-    const label = await browser.getElementText(todoCheckboxLabel[1]['element-6066-11e4-a52e-4f735466cecf'])
 
+    await browser.elementClick(todoCheckbox[1]['element-6066-11e4-a52e-4f735466cecf'])
+
+    const label = await browser.getElementText(todoCheckboxLabel[1]['element-6066-11e4-a52e-4f735466cecf'])
     const isClicked = await browser.isElementSelected(todoCheckbox[1]['element-6066-11e4-a52e-4f735466cecf'])
     const todoItemsLeft = todoCheckbox.length - 1;
-    
+
     console.log(`todo item ${label} is clicked === ${isClicked}`)
     console.log(`todo items left === ${todoItemsLeft}`)
-    
+
     await browser.deleteSession()
 
 })().catch(async (e) => {
