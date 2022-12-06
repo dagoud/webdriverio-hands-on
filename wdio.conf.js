3f0fc0dec0728273e70aef10cefc8366623b1f40
dagoud <dagoudie@gmx.com>
2022-12-06 00:06:16 +0000

    DG Chapter 4 complete

--- wdio.conf.js
+++ wdio.conf.js
@@ -75,6 +75,7 @@ exports.config = {
     // ===================
     // Define all options that are relevant for the WebdriverIO instance here
     //
+    outputDir: 'logs',
     // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'info',
     //
@@ -116,7 +117,13 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: [
+        ['chromedriver', {
+            logFileName: 'wdio-chromedriver.log', // default
+            outputDir: 'logs/chromedriver-logs', // overwrites the config.outputDir
+            // args: ['--silent']
+        }]
+      ],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
